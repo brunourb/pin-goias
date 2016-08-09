7cdd217c8aa76fee16b6c64d21c140a1c7318109
Elio Kenta Numazaki <elio-kn@segplan.go.gov.br>
2016-08-09 11:43:20 -0300

    Alteração para que as URLs sejam obtidas dinamicamente.

--- pin-api/src/main/java/gov/goias/sistema/api/conf/Bootstrap.java
+++ pin-api/src/main/java/gov/goias/sistema/api/conf/Bootstrap.java
@@ -1,5 +1,6 @@
 package gov.goias.sistema.api.conf;
 
+import io.swagger.config.ConfigFactory;
 import io.swagger.jaxrs.config.SwaggerContextService;
 import io.swagger.models.Contact;
 import io.swagger.models.Info;
@@ -15,14 +16,17 @@ public class Bootstrap extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         ServletContext context = config.getServletContext();
+
+        // Inicializa o Swagger
         Info info = new Info()
                 .title("Padrão de Interoperabilidade - Goiás")
                 .description("Padrão de interoperabilidade para os serviços disponibilizados.")
                 .termsOfService("")
                 .contact(new Contact()
-                        .email("projetos.scti@segplan.go.gov.br"));
+                        .email("projetos.scti@segplan.go.gov.br")).version("1.0.0");
 
         Swagger swagger = new Swagger().info(info);
+        swagger.setBasePath(context.getContextPath() + "/api");
 
         new SwaggerContextService().withServletConfig(config).updateSwagger(swagger);
     }
